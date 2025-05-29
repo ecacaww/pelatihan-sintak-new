73d613e665fac47b0765ced4813651f54c60803d
zapie89 <zafirahputri11@gmail.com>
2025-05-29 22:15:24 +0700

    10

--- src/Pages/NavBar.jsx
+++ src/Pages/NavBar.jsx
@@ -25,7 +25,7 @@ function NavBar() {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-stone-800 rounded-box w-52 border border-stone-700"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-stone-800 rounded-box w-52 border border-stone-700 "
           >
             <li>
               <Link to="/" className="hover:text-amber-300">
@@ -55,11 +55,11 @@ function NavBar() {
         </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 gap-2">
+        <ul className="menu menu-horizontal px-1 gap-2 text-[#333333]">
           <li>
             <Link
               to="/"
-              className="font-medium text-stone-300 hover:text-amber-300 hover:bg-stone-800/50 rounded-lg px-4 py-2 transition-colors"
+              className="font-medium  hover:text-amber-300 hover:bg-stone-800/50 rounded-lg px-4 py-2 transition-colors"
             >
               Home
             </Link>
@@ -67,7 +67,7 @@ function NavBar() {
           <li>
             <Link
               to="/about"
-              className="font-medium text-stone-300 hover:text-amber-300 hover:bg-stone-800/50 rounded-lg px-4 py-2 transition-colors"
+              className="font-medium  hover:text-amber-300 hover:bg-stone-800/50 rounded-lg px-4 py-2 transition-colors"
             >
               About Us
             </Link>
@@ -75,7 +75,7 @@ function NavBar() {
           <li>
             <Link
               to="/product"
-              className="font-medium text-stone-300 hover:text-amber-300 hover:bg-stone-800/50 rounded-lg px-4 py-2 transition-colors"
+              className="font-medium  hover:text-amber-300 hover:bg-stone-800/50 rounded-lg px-4 py-2 transition-colors"
             >
               Mochi Products
             </Link>
@@ -83,7 +83,7 @@ function NavBar() {
           <li>
             <Link
               to="/contact"
-              className="font-medium text-stone-300 hover:text-amber-300 hover:bg-stone-800/50 rounded-lg px-4 py-2 transition-colors"
+              className="font-medium  hover:text-amber-300 hover:bg-stone-800/50 rounded-lg px-4 py-2 transition-colors"
             >
               Contact
             </Link>
