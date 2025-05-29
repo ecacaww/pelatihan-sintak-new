1d7bd2a7264a3823b6e85b0333fe9c10b70f5e8b
zapie89 <zafirahputri11@gmail.com>
2025-05-30 05:34:57 +0700

    51

--- src/Pages/NavBar.jsx
+++ src/Pages/NavBar.jsx
@@ -122,7 +122,32 @@ function NavBar() {
               <span className="text-lg font-bold">8 Items</span>
               <span className="text-info">Subtotal: $999</span>
               <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
+                {/* Open the modal using document.getElementById('ID').showModal() method */}
+                <button
+                  className="btn"
+                  onClick={() =>
+                    document.getElementById("my_modal_5").showModal()
+                  }
+                >
+                  View Cart
+                </button>
+                <dialog
+                  id="my_modal_5"
+                  className="modal modal-bottom sm:modal-middle"
+                >
+                  <div className="modal-box">
+                    <h3 className="font-bold text-lg">Hello!</h3>
+                    <p className="py-4">
+                      Press ESC key or click the button below to close
+                    </p>
+                    <div className="modal-action">
+                      <form method="dialog">
+                        {/* if there is a button in form, it will close the modal */}
+                        <button className="btn">Close</button>
+                      </form>
+                    </div>
+                  </div>
+                </dialog>
               </div>
             </div>
           </div>
