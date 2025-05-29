b9cbe3fbc75c60a19064a1a1100a507cc7e1e9e8
zapie89 <zafirahputri11@gmail.com>
2025-05-30 06:13:19 +0700

    56

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css"
+import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
@@ -6,23 +6,22 @@ import AboutPage from "./Pages/AboutPage";
 import ProductPage from "./Pages/ProductPage";
 import ContactPage from "./Pages/ContactPage";
 import NotFoundPage from "./Pages/NotFoundPage";
-
+import { CartProvider } from "./CartContext";
 
 function App() {
   return (
     <>
-
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage/>}  />
-          <Route path="/about" element={<AboutPage/>}  />
-          <Route path="/product" element={<ProductPage/>}  />
-          <Route path="/contact" element={<ContactPage/>}  />
-          <Route path="*" element={<NotFoundPage/>}  />
-        </Routes>
-      </Router>
-      
-      
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/product" element={<ProductPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Router>
+      </CartProvider>
     </>
   );
 }
--- src/CartContext.js
+++ src/CartContext.js
@@ -1,4 +1,4 @@
-// CartContext.js
+// src/context/CartContext.js
 import React, { createContext, useState, useContext } from "react";
 
 const CartContext = createContext();
@@ -41,16 +41,19 @@ export function CartProvider({ children }) {
     }
   };
 
-  const value = {
-    cartItems,
-    subtotal,
-    addToCart,
-    removeFromCart,
-  };
-
-  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider
+      value={{ cartItems, subtotal, addToCart, removeFromCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
 
 export function useCart() {
-  return useContext(CartContext);
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
 }
